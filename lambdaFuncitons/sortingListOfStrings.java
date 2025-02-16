1f1b762293a115be0118d90c5e3c69d46d6fd90a
mbank.deepan <mbank.deepan@gmail.com>
2025-02-16 16:32:27 +0545

    upto qno17 done

--- lambdaFuncitons/sortingListOfStrings.java
+++ lambdaFuncitons/sortingListOfStrings.java
@@ -1,7 +1,7 @@
 package lambdaFuncitons;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 class sortingListOfStrings {
@@ -16,10 +16,16 @@ class sortingListOfStrings {
         // code below will actually create a new list
         // List<String> testList = new ArrayList<>(namesList);
 
+        // ASCENDING ORDER
         // testList.sort((str1, str2) -> str1.compareTo(str2));
-        testList.sort(String::compareTo);
+        // testList.sort(String::compareTo);
 
-        System.out.println(namesList);
+        // DESCENDING ORDER
+        // testList.sort((s1, s2) -> s2.compareTo(s1));
+        // other approach for descending
+        testList.sort(Comparator.reverseOrder());
+
+        // System.out.println(namesList);
         System.out.println(testList);
     }
 }
