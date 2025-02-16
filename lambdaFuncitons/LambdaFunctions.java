2910addae7fb3b51fa7b140fa017ec7a98d9dd45
mbank.deepan <mbank.deepan@gmail.com>
2025-02-16 16:47:13 +0545

    qno18 done 19 remain

---
+++ lambdaFuncitons/LambdaFunctions.java
@@ -0,0 +1,23 @@
+package lambdaFuncitons;
+
+import java.util.Scanner;
+
+public class LambdaFunctions {
+    public interface LambdaFunctionsTest {
+        boolean checkEmpty(String string);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter a string to test if it's empty");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        // String testString = "Ram";
+        LambdaFunctionsTest lambdaFunctionsTest = (string -> {
+            System.out.println(string.getClass());
+            return string.isEmpty();
+        });
+        boolean result = lambdaFunctionsTest.checkEmpty(userInput);
+
+        System.out.println(result);
+    }
+}
---
+++ lambdaFuncitons/qno19.java
@@ -0,0 +1,8 @@
+package lambdaFuncitons;
+
+// 19. Write a Java program to implement a lambda expression to find the second largest and smallest element in an array. 
+public class qno19 {
+    public static void main(String[] args) {
+
+    }
+}
